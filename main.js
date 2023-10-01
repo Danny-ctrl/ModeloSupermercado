804c65170e9fc671b2061f60d14c7c525f37ca6f
Daniele Freitas <71996747+Danny-ctrl@users.noreply.github.com>
2023-10-01 18:40:57 -0300

    material icon

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
 function carImagem(){
   document.getElementById("imagem").innerHTML="<img src='assets/jornalzinho.jpg'>";
 }
+/* abre e fecha o menu quando clicar no card e no icone x*/
+
+const section = document.querySelector('#main card ')
+const toggle = document.querySelectorAll('card .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+/*quando clicar em um item do card,esconder o card*/
+const links = document.querySelectorAll('main section nome Card card')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
